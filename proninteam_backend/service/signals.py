307032bdb4d6443331706a74154c4449d430bf76
Anton Lebedev <wrawka@gmail.com>
2025-04-24 21:15:28 +0700

    clean-up

--- proninteam_backend/service/signals.py
+++ proninteam_backend/service/signals.py
@@ -8,14 +8,12 @@ from service.tasks import send_collection_created_email, send_payment_created_em
 @receiver(post_save, sender=Collection)
 @receiver(post_delete, sender=Collection)
 def invalidate_collection_cache(sender, instance, **kwargs):
-    print(f"Invalidating cache for {sender.__name__} with ID {instance.id}")
     caches["api"].clear()
 
 
 @receiver(post_save, sender=Payment)
 @receiver(post_delete, sender=Payment)
 def invalidate_payment_cache(sender, instance, **kwargs):
-    print(f"Invalidating cache for {sender.__name__} with ID {instance.id}")
     caches["api"].clear()
 
 
