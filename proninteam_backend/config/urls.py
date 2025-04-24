f5f8929928bb473184871f7b65279813271b4583
Anton Lebedev <wrawka@gmail.com>
2025-04-24 19:22:03 +0700

    urls clean-up

--- proninteam_backend/config/urls.py
+++ proninteam_backend/config/urls.py
@@ -1,27 +1,13 @@
-"""
-URL configuration for proninteam_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     # Admin panel
     path("admin/", admin.site.urls),
+    # Redirect root URL to API endpoint
+    path("", RedirectView.as_view(url="/api/schema/swagger-ui/", permanent=False)),
     # API endpoints
     path("api/", include(("api.urls", "api"), namespace="api")),
     # API schema generation
