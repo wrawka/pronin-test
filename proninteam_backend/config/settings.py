f4918dcf5f0fb8092183c9e49e458b479a77bdf1
Anton Lebedev <wrawka@gmail.com>
2025-04-24 21:13:32 +0700

    setup test email backend

--- proninteam_backend/config/settings.py
+++ proninteam_backend/config/settings.py
@@ -147,6 +147,10 @@ STORAGES = {
     },
 }
 
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "emails")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
